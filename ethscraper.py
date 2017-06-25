1bd0d6a2631970a425ce569afda1718e62f66ba9
Julian Prester <julian.prester@gmail.com>
2017-06-25 15:20:32 +0200

    Include python scraper script

---
+++ ethscraper.py
@@ -0,0 +1,96 @@
+import requests
+import pandas as pd
+
+output_dir = './data/'
+base_url ='http://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&endblock=99999999&sort=asc'
+address = '0x687aeda127fd2bd4f374c73e3724bf9b7c7a6b9c'
+
+def get_transactions(addr):
+    """Make an Etherscan API request convert the resulting JSON response to a Pandas dataframe
+
+    Parameters
+    ----------
+    addr : string
+        The ETH address which transactions are to be analyzed
+
+    Returns
+    -------
+    df : Pandas dataframe
+        The Etherscan API response as a Pandas dataframe
+    """
+    r = requests.get(base_url.format(addr))
+    return pd.DataFrame(r.json()['result'])
+
+def preprocess(df):
+    """Preprocess the transaction data before saving to csv files
+
+    Parameters
+    ----------
+    df : Pandas dataframe
+        The Etherscan API response as a Pandas dataframe
+
+    Returns
+    -------
+    df : Pandas dataframe
+        The preprocessed Pandas dataframe
+    """
+    df.drop(df.columns[[0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 15]], axis=1, inplace=True)
+
+    df['value'] = pd.to_numeric(df['value'], errors='coerce').astype(float)
+    df['value'] = df['value'] / 1000000000000000000
+
+    df['timeStamp'] = pd.to_datetime(df['timeStamp'], unit='s').dt.date
+    df.columns = ['from', 'date', 'to', 'value']
+    df = df[['date', 'from', 'to', 'value']]
+    return df
+
+def build_incoming_data(df):
+    """Build the dataframe for the incoming transactions plot
+
+    Parameters
+    ----------
+    df : Pandas dataframe
+        The preprocessed Pandas dataframe
+    """
+    df = df.where(df['to'] == address).loc[:, ['date', 'value']]
+    df = df.groupby('date', as_index=False).sum()
+    df['cumulative'] = df.value.cumsum()
+    df.to_csv(output_dir + 'incoming.csv', index=False)
+
+def build_outgoing_data(df):
+    """Build the dataframe for the outgoing transactions plot
+
+    Parameters
+    ----------
+    df : Pandas dataframe
+        The preprocessed Pandas dataframe
+    """
+    df = df.where(df['from'] == address).loc[:, ['date', 'value']]
+    df = df.groupby('date', as_index=False).sum()
+    df['cumulative'] = df.value.cumsum()
+    df.to_csv(output_dir + 'outgoing.csv', index=False)
+
+def build_address_data(df):
+    """Build the dataframe for the unique addresses plot
+
+    Parameters
+    ----------
+    df : Pandas dataframe
+        The preprocessed Pandas dataframe
+    """
+    df = df.where(df['to'] == address).loc[:, ['date', 'from']]
+    df = df.groupby('from', as_index=False).min()
+    df = df.groupby('date', as_index=False).count()
+    df.columns = ['date', 'value']
+    df['cumulative'] = df.value.cumsum()
+    df.to_csv(output_dir + 'address.csv', index=False)
+
+def main():
+    df = get_transactions(address)
+    df = preprocess(df)
+    build_incoming_data(df)
+    build_outgoing_data(df)
+    build_address_data(df)
+
+if __name__ == '__main__':
+    main()
